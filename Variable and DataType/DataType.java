f3eec01df9fce8194b4227dc67ea7caf76b8435b
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:39:57 +0530

    Create DataType.java

---
+++ Variable and DataType/DataType.java
@@ -0,0 +1,18 @@
+public class DataType {
+    public static void main(String args[]) {
+       byte b = 8;
+       System.out.println(b);
+       char ch = 'a';
+       System.out.println(ch);
+       double p = 9.7;
+       System.out.println(p);
+       int n = 22;
+       System.out.println(n);
+       //long
+       //double
+       //boolean
+       short m = 550;
+       System.out.println(m);
+
+    }
+}
