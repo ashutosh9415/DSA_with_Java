a94eb3cc9179059abd96dedc4b8700cecfd2fda2
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:26:13 +0530

    Create switchStatements.java

---
+++ Conditional Statements/switchStatements.java
@@ -0,0 +1,14 @@
+public class switchStatements{
+    public static void main(String args[]){
+        int num = 0;
+        switch(num){
+            case 1 : System.out.println("samosa");
+                break;
+            case 2 : System.out.println("Pizza");
+                break;
+            case 3 : System.out.println("mango shake");
+                break;
+            default: System.out.println("we wake up");
+        }
+    }
+}
