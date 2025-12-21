2cdb2dcc0b7e1513d0d3bf38f1c84898b3ae4a05
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:21:45 +0530

    Create nQueenCountWays.java

---
+++ BackTracking/nQueenCountWays.java
@@ -0,0 +1,65 @@
+public class nQueenCountWays {
+
+    static int count = 0;
+    public static boolean isSafe(char board[][], int row, int col) {
+        // check column
+        for (int i = 0; i < row; i++) {
+            if (board[i][col] == 'Q')
+            return false;
+        }
+
+        // left diagonal
+        for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+            return false;
+        }
+
+        // right diagonal
+        for (int i = row, j = col; i >= 0 && j < board.length; i--, j++) {
+            if (board[i][j] == 'Q')
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void nQueen(char board[][], int row) {
+        if (row == board.length) {
+            count++;
+            return;
+        }
+
+        for (int j = 0; j < board.length; j++) {
+            if (isSafe(board, row, j)) {   // check if it's safe to place queen
+                board[row][j] = 'Q';
+                nQueen(board, row + 1);   // recursive call
+                board[row][j] = 'x';      // backtrack
+            }
+        }
+    }
+
+    public static void printBoard(char board[][]) {
+        System.out.println("-----Chess Board-----");
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        char board[][] = new char[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = 'x';
+            }
+        }
+
+        nQueen(board, 0);
+        System.out.println("Total ways to place " + n + " queens: " + count);
+    }
+    
+}
