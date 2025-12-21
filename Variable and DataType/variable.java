32c2ea849bbf894c44b0d1a306e123bbc177f2e7
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:40:05 +0530

    Create variable.java

---
+++ Variable and DataType/variable.java
@@ -0,0 +1,15 @@
+public class variable {
+    public static void main(String args[]) {
+        int a= 50;
+        int b = 60;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Ashutosh";
+        System.out.println(name);
+
+        a = b;
+        System.out.println(a);
+        
+        
+    }
+}        
