74cb80a728a49491210619ac90368160237f4c18
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:30:29 +0530

    Create Print1To10.java

---
+++ Loops(FlowControl)/Print1To10.java
@@ -0,0 +1,12 @@
+public class Print1To10{
+    public static void main(String args[]){
+
+        int count = 1;
+        System.out.print("your 10 number:");
+        while(count <= 10){
+            System.out.print(" "+ count);
+            count++;
+        }
+        System.out.println();
+    }
+}
