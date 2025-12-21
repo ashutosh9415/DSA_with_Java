28fa26fbb7e475c3eed0580f55637e68395c94db
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:27:55 +0530

    Create FunctionOverloading.java

---
+++ Function & Methods/FunctionOverloading.java
@@ -0,0 +1,20 @@
+public class FunctionOverloading{
+
+    public static int sum(int a, int b){
+        return a+b;
+    }
+    
+    public static float sum(float a, float b){
+        return a+b;
+    }
+
+    public static int sum(int a, int b, int c){
+        return a+b+c;
+    }
+
+    public static void main(String args[]){
+        System.out.println(sum(3,5));
+        System.out.println(sum(3.1f,5.6f));
+        System.out.println(sum(4,8,7));
+    }
+}
