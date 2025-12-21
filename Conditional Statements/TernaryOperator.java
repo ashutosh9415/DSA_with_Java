e995037ac8f7e0d83a8b611178b50aa38631f862
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:26:25 +0530

    Create TernaryOperator.java

---
+++ Conditional Statements/TernaryOperator.java
@@ -0,0 +1,9 @@
+public class TernaryOperator{
+    public static void main(String args[]){
+        int num = 89;
+
+        //ternary Operator
+        String type =((num % 2)==0) ? "even":"odd";
+        System.out.println(type);
+    }
+}
