d875ec245c16bd732a6d39716362672fe44787dd
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:23:23 +0530

    Create BubbleSort.java

---
+++ Basic Sorting Algorithams/BubbleSort.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class BubbleSort {
+    public static void bubbleSort(int arr[]) {
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArrSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int Array[] = new int[n];
+        System.out.print("Enter the elements in array:");
+        for (int i = 0; i < n; i++) {
+            Array[i] = sc.nextInt();
+        }
+
+        bubbleSort(Array);
+        System.out.print("Sorted array: ");
+        printArrSort(Array);
+    }
+}
