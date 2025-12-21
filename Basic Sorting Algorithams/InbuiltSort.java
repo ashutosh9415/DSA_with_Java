46efb086d2cc3be9455eafb06f20c9e5d8944f71
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:26:39 +0530

    Create InbuiltSort.java

---
+++ Basic Sorting Algorithams/InbuiltSort.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.Arrays;
+
+public class InbuiltSort {
+
+    public static void printArrSort(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int Array[] = new int[n];
+        System.out.print("Enter the elements in array:");
+        for (int i = 0; i < n; i++) {
+            Array[i] = sc.nextInt();
+        }
+
+        // Arrays.sort(Array);
+
+        Arrays.sort(Array,0,3);
+        System.out.print("Sorted array: ");
+        printArrSort(Array);
+    }
+}
