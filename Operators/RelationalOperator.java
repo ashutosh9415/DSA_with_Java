c574dbaae4fad9e8ef13e9a094ef0eb87eb6035c
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:34:44 +0530

    Create RelationalOperator.java

---
+++ Operators/RelationalOperator.java
@@ -0,0 +1,30 @@
+public class RelationalOperator{
+    public static void main(String args[]){
+
+        int a=10;      //equal to operator
+        int b=10;
+        System.out.println(a==b);
+
+
+        int c=10;       // not  equal to operator
+        int d=10;
+        System.out.println(c!=d);
+
+        int e=10;      //rgreater than operator
+        int f=5;
+        System.out.println(e>f);
+
+        int h=9;        //less than operator
+        int i=10;
+        System.out.println(h<i);
+
+
+        int j=10;         //rgreater than equal to operator
+        int k=10;
+        System.out.println(j>=k);
+
+        int m=10;            //less than equal to operator
+        int n=10;
+        System.out.println(m<=n);
+    }
+}
