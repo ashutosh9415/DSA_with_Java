33a6bb3cf6e06f4a47a12848c815d99ce9f6f670
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:30:38 +0530

    Create SquarePrint.java

---
+++ Loops(FlowControl)/SquarePrint.java
@@ -0,0 +1,13 @@
+public class SquarePrint{
+    public static void main(String args[]){
+        // for (int i =1; i<=4;i++){
+        //     System.out.println("* * * *");
+        // }
+
+        int i=0;
+        while (i<=4){
+            System.out.println("* * * *");
+            i++;
+        }
+    }
+}
