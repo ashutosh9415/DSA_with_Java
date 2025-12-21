f100f9384c44d41e95689dee04471146ab3bbd21
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:34:35 +0530

    Create logicalOperator.java

---
+++ Operators/logicalOperator.java
@@ -0,0 +1,10 @@
+public class logicalOperator{
+    public static void main(String args[]){
+
+        System.out.println((3>4)&&(5>2));   //logical AND
+
+        System.out.println((3>4) || (5>2));   //logical OR
+
+        System.out.println(!(5>2));    // logial NOT
+    }
+}
