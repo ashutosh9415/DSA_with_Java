7da87ea6de16b2bb41c35cf63c95a1e5b90b37ca
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:30:40 +0530

    Create PrimeOrNot.java

---
+++ Loops(FlowControl)/PrimeOrNot.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class PrimeOrNot {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        
+        if (num == 2) {
+            System.out.println("Number is prime.");
+        } else {
+            boolean isPrime = true;
+            for (int i = 2; i <(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println("Number is prime.");
+            } else {
+                System.out.println("Number is not prime.");
+            }
+        }
+    }
+}
