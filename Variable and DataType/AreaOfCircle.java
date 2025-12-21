9cb57a76343cfc18dfabbb2b88e886ba57668a44
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:39:20 +0530

    Create AreaOfCircle.java

---
+++ Variable and DataType/AreaOfCircle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class AreaOfCircle{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter redius of circle : ");
+        float r = sc.nextFloat();
+        float Area = 3.14f * r * r;
+        System.out.println("Area of circle is: " + Area);
+    }
+}
