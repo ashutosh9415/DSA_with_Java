dc622dca3ed4b9dee1ce5b056e01a7751cd0e827
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:34:30 +0530

    Create AssignmentOperator.java

---
+++ Operators/AssignmentOperator.java
@@ -0,0 +1,24 @@
+public class AssignmentOperator{
+    public static void main(String args[]){
+         
+        int A = 5;
+        A+=5;    //A=A+5
+        System.out.println(A);
+
+        int B = 5;
+        B-=5;  //B=B-5
+        System.out.println(B);
+
+        int C = 5;
+        C*=5;   //C=C*5
+        System.out.println(C);
+
+        int D = 5;
+        D/=5;      //D=D/5
+        System.out.println(D);
+
+        int E = 5;
+        E%=5;    //E=E%5
+        System.out.println(E);
+    }
+}
