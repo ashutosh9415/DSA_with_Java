d6589c89c76bd4c189e1ecbcfcf7ece0723afa12
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:25:27 +0530

    Create UpdateIthBits.java

---
+++ Bit Manipulation/UpdateIthBits.java
@@ -0,0 +1,32 @@
+public class UpdateIthBits {
+    // public static int updateIthBit(int n,int i,int newBit) {
+    //     if (newBit == 0) {
+    //         return clearIthBit(n, i);
+    //     } else {
+    //         return setIthBit(n, i);
+    //     }
+    // }
+    // public static int clearIthBit(int n,int i) {
+    //     int bitMask = ~(1 << i);
+    //     return n & bitMask;
+    // }
+    // public static int setIthBit(int n,int i) {
+    //     int bitMask = 1 << i;
+    //     return n | bitMask;
+    // }
+    
+    public static int updateIthBit(int n,int i,int newBit) {
+        n = clearIthBit(n, i);
+        int bitMask = newBit << i;
+        return n | bitMask;
+    }
+    public static int clearIthBit(int n,int i) {
+        int bitMask = ~(1 << i);
+        return n & bitMask;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(updateIthBit(10,1,0));
+    }
+    
+}
