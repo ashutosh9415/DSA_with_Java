8c30f37f1c27d59816cb9722a1df0e771b9fbac2
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2025-12-22 00:31:36 +0530

    Create WhileLoops.java

---
+++ Loops(FlowControl)/WhileLoops.java
@@ -0,0 +1,11 @@
+public class WhileLoops{
+    public static void main(String args[]){
+        
+        int i=0;
+        while (i<10){
+            System.out.println("Hello world!");
+            i++;
+        }
+        System.out.println("Printed hello world 100X.");
+    }
+}
