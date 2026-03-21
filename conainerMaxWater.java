c9361d188bff8bbd69beade1c3116361a70dab5c
Ashutosh Vishwakarma <vashutosh236@gmail.com>
2026-03-21 11:54:57 +0530

    Add files via upload

---
+++ conainerMaxWater.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class conainerMaxWater {
+    public static void main(String[] args) {
+        ArrayList<Integer> height = new ArrayList<>();
+        //1,8,6,2,5,4,8,3,7
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+
+        System.out.println(maxWater(height));
+    }
+
+    public static int maxWater(ArrayList<Integer> height){
+
+        int tWater = 0;
+        for(int i=0;i<height.size();i++){
+            for(int j=i+1;j<height.size();j++){
+                int tHig = Math.min(height.get(i),height.get(j));
+                int width = j-i;
+                int currWater = tHig*width;
+                tWater = Math.max(tWater, currWater);
+            }
+        }
+        return tWater;
+    }
+    
+}
